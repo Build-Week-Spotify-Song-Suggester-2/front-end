b7588b003970e973251add47d45e482353131824
Jason <jason.rostar@yahoo.com>
2020-05-26 12:45:51 -0700

    added some styling to navigation

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom"
-
+import './styles.navigation.scss'
 
 
 function Navigation() {
     return (
-      <div>     
-        <Link to={"/register"}>Register</Link>
-            <br></br>
-            <br></br>
-        <Link to={"/login"}>Login</Link>
-            <br></br>
-            <br></br>
-        <Link to={"/"}>Home</Link>
-            <br></br>
-            <br></br>
-        <Link to={"/search"}>Search</Link>
+      <div className='navigation'>  
+      <Link to={"/"}>Home</Link>   
+          <br></br>
+          <br></br>
+      <Link to={"/search"}>Search</Link>  
+          <br></br>
+          <br></br>
+      <Link to={"/register"}>Register</Link>
+          <br></br>
+          <br></br>
+      <Link to={"/login"}>Login</Link>
+          <br></br>
+          <br></br>
       </div>
     );
   }
